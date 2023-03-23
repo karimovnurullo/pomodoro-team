ef6c1401f30879dc87f7eea5cfeea095ac99fd1a
Nurullo <karimovdeveloper@gmail.com>
2023-03-23 13:59:32 +0500

    js code added

--- app.js
+++ app.js
@@ -7,3 +7,19 @@ const startBtn = document.getElementById("startBtn");
 
 let time = 1500;
 let intervalID;
+// ui functions
+function handleStart() {
+	const isActive = startBtn.classList.contains("active");
+	startBtn.classList.toggle("active");
+	startBtn.innerText = isActive ? "START" : "PAUSE";
+
+	const value = timeConvertor(time);
+	if (isActive) {
+		clearInterval(intervalID);
+	} else {
+		intervalID = setInterval(() => {
+			time--;
+			timer.innerText = timeConvertor(time);
+		}, 1000);
+	}
+}
